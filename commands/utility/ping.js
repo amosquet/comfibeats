37bdaf4643ee8bf5b7a228ab94977aafc06337b1
amosquet <artusmosquet@gmail.com>
2025-12-12 20:35:48 -0500

    did some stuff

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     cooldown: 5,
-	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Ping!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply(`Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms`);
 	},
 };
