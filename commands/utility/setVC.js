d3534f4623eb85edb67c5613eb156bd36decac03
madiexe <madelinehess42@gmail.com>
2025-12-20 17:50:13 -0500

    vcId written to config.json when command is invoked !

--- commands/utility/setVC.js
+++ commands/utility/setVC.js
@@ -1,4 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+
+// config file
+const config = require('../../config.json');
+
+console.log(config);
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,7 +16,16 @@ module.exports = {
             .setRequired(true)),
     async execute(interaction) {
         const channelId = interaction.options.getString('channelid');
-        //Have to save the channelId to a database or configuration file
-        await interaction.reply(`Voice channel set to ID: \`${channelId}\``);
+        
+        // set and write channel/vc id to config
+        config.vcId = channelId
+        const json = JSON.stringify(config);
+
+        try {
+          fs.writeFileSync('./config.json', json);
+          await interaction.reply(`Voice channel set to ID: \`${channelId}\``);
+        } catch (err) {
+          console.log(`error writing config :< !`, err);
+        }
     }
 }
